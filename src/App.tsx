5cf782c0958c4325b031eefd3215d8bb7452735d
Kirill <kirill-782@yandex.ru>
2022-04-08 19:57:09 +0300

    Router refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useReducer, useState } from "react";
-import { SemanticToastContainer } from "react-semantic-toasts";
-import { Icon } from "semantic-ui-react";
-import Footer from "./components/Footer";
-import Header, { MenuItem } from "./components/Header";
 import { AppRuntimeSettingsContext, WebsocketContext } from "./context";
 import { GHostWebSocket } from "./services/GHostWebsocket";
 
 import "react-semantic-toasts/styles/react-semantic-alert.css";
-import PageContent from "./components/PageContent";
-
-const setTheme = (theme: string) => {
-  localStorage.setItem("theme", theme);
-  document.location.reload();
-};
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import GameListPage from "./components/Pages/GameListPage";
 
 function App() {
   useEffect(() => {
@@ -30,97 +23,6 @@ function App() {
       };
     }
   }, []);
-
-  const menuItems: Array<MenuItem> = [
-    {
-      type: "item",
-      name: "logo",
-      text: "Справка",
-      node: (
-        <span>
-          IrInAB
-          <Icon name="circle" />T
-        </span>
-      ),
-      onClick: (a, b) => console.log(a, b),
-    },
-    {
-      type: "item",
-      name: "gamelist",
-      icon: "gamepad",
-      text: "Активные игры",
-      node: <span>Активные игры</span>,
-      onClick: (a, b) => console.log(a, b),
-    },
-    {
-      type: "item",
-      name: "help",
-      icon: "help",
-      text: "Справка",
-      node: <span>Справка</span>,
-      onClick: (a, b) => console.log(a, b),
-    },
-    {
-      type: "item",
-      name: "mapupload",
-      icon: "upload",
-      text: "Загрузка карт",
-      node: <span>Загрузка карт</span>,
-      position: "right",
-      onClick: (a, b) => console.log(a, b),
-    },
-    {
-      type: "menu",
-      name: "usermenu",
-      text: "Symmetra",
-      node: (
-        <span>
-          <Icon name="user" />
-          Symmetra
-        </span>
-      ),
-      onClick: (a, b) => console.log(a, b),
-      subMenu: [
-        {
-          type: "item",
-          name: "donates",
-          icon: "ruble sign",
-          text: "Загрузка карт",
-          node: <span>Донателло</span>,
-        },
-      ],
-    },
-    {
-      type: "menu",
-      name: "theme",
-      text: "Выбор темы",
-      node: (
-        <span>
-          <Icon name="paint brush" />
-          Выбор темы
-        </span>
-      ),
-      onClick: (...args) => console.log("theme", ...args),
-      subMenu: [
-        {
-          type: "item",
-          name: "dark",
-          icon: "sun",
-          text: "Тёмная тема",
-          node: <span>Тёмная тема</span>,
-          onClick: () => setTheme("dark"),
-        },
-        {
-          type: "item",
-          name: "light",
-          icon: "sun outline",
-          text: "Светлая тема",
-          node: <span>Светлая тема</span>,
-          onClick: () => setTheme("light"),
-        },
-      ],
-    },
-  ];
 
   const [ghostSocket, dispatchGHostSocket] = useReducer((state, action) => {
     if (action === "connect") state.connect();
@@ -148,10 +50,12 @@ function App() {
           gameList: { locked: gameListLocked, setLocked: setGameListLocked },
         }}
       >
-        <Header items={menuItems}></Header>
-        <SemanticToastContainer animation="fade" position="top-right" />
-        <PageContent />
-        <Footer></Footer>
+        <Routes>
+          <Route path="/*" element={<Layout />}>
+            <Route index element={<GameListPage />} />
+            <Route path="gamelist" element={<GameListPage />} />
+          </Route>
+        </Routes>
       </AppRuntimeSettingsContext.Provider>
     </WebsocketContext.Provider>
   );
---
+++ src/components/Layout.tsx
@@ -0,0 +1,116 @@
+import { Outlet } from "react-router-dom";
+import { SemanticToastContainer } from "react-semantic-toasts";
+import { Icon } from "semantic-ui-react";
+import Footer from "./Footer";
+import Header, { MenuItem } from "./Header";
+
+const setTheme = (theme: string) => {
+  localStorage.setItem("theme", theme);
+  document.location.reload();
+};
+
+const menuItems: Array<MenuItem> = [
+  {
+    type: "item",
+    name: "logo",
+    text: "Справка",
+    node: (
+      <span>
+        IrInAB
+        <Icon name="circle" />T
+      </span>
+    ),
+    onClick: (a, b) => console.log(a, b),
+  },
+  {
+    type: "item",
+    name: "gamelist",
+    icon: "gamepad",
+    text: "Активные игры",
+    node: <span>Активные игры</span>,
+    onClick: (a, b) => console.log(a, b),
+  },
+  {
+    type: "item",
+    name: "help",
+    icon: "help",
+    text: "Справка",
+    node: <span>Справка</span>,
+    onClick: (a, b) => console.log(a, b),
+  },
+  {
+    type: "item",
+    name: "mapupload",
+    icon: "upload",
+    text: "Загрузка карт",
+    node: <span>Загрузка карт</span>,
+    position: "right",
+    onClick: (a, b) => console.log(a, b),
+  },
+  {
+    type: "menu",
+    name: "usermenu",
+    text: "Symmetra",
+    node: (
+      <span>
+        <Icon name="user" />
+        Symmetra
+      </span>
+    ),
+    onClick: (a, b) => console.log(a, b),
+    subMenu: [
+      {
+        type: "item",
+        name: "donates",
+        icon: "ruble sign",
+        text: "Загрузка карт",
+        node: <span>Донателло</span>,
+      },
+    ],
+  },
+  {
+    type: "menu",
+    name: "theme",
+    text: "Выбор темы",
+    node: (
+      <span>
+        <Icon name="paint brush" />
+        Выбор темы
+      </span>
+    ),
+    onClick: (...args) => console.log("theme", ...args),
+    subMenu: [
+      {
+        type: "item",
+        name: "dark",
+        icon: "sun",
+        text: "Тёмная тема",
+        node: <span>Тёмная тема</span>,
+        onClick: () => setTheme("dark"),
+      },
+      {
+        type: "item",
+        name: "light",
+        icon: "sun outline",
+        text: "Светлая тема",
+        node: <span>Светлая тема</span>,
+        onClick: () => setTheme("light"),
+      },
+    ],
+  },
+];
+
+function Layout() {
+  return (
+    <>
+      <Header items={menuItems}></Header>
+      <SemanticToastContainer animation="fade" position="top-right" />
+      <div style={{ marginTop: 50 }}>
+        <Outlet />
+      </div>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default Layout;
