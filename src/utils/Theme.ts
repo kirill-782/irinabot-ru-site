234f899611e2fc29f2d379aa44e414fcd2fc46c5
Qoo <2015abidin@gmail.com>
2022-04-10 17:27:32 +0300

    Implement dark mode

---
+++ src/utils/Theme.ts
@@ -0,0 +1,33 @@
+export enum E_THEME {
+  LIGHT =  'light',
+  DARK = 'dark',
+}
+
+const themeKeyInLocalStorage = 'theme';
+
+
+/** Получение текущей темы */
+export const getTheme = (): E_THEME => localStorage.getItem(themeKeyInLocalStorage) as E_THEME || E_THEME.LIGHT;
+
+/** Загрузка темы из semantic-ui-sass
+ *
+ * @param {E_THEME} themeName Название темы (dark, light и т.д)
+ */
+export const loadTheme = () => {
+  const theme = getTheme();
+  console.log('theme? ', theme);
+  if (theme === E_THEME.LIGHT) {
+    return;
+  }
+  require(`../semantic-ui-sass/${theme}/_index.scss`);
+};
+
+/** Переключение темы сайта
+ *
+ * @param {E_THEME} themeName Название темы (dark, light и т.д)
+ */
+export const switchTheme = (themeName: E_THEME) => {
+  console.log('switch theme', themeName);
+  localStorage.setItem(themeKeyInLocalStorage, themeName);
+  document.location.reload();
+};
