eb1a79ca2655ef840387a44f9dcb2d09b1da9443
Qoo <2015abidin@gmail.com>
2022-04-18 20:02:21 +0300

    Implement drag and drop uploader

---
+++ src/components/Modal/DragAndDropField.tsx
@@ -0,0 +1,7 @@
+import './DragAndDropField.scss';
+
+export const DragAndDropField = () => {
+    return <div className='drag-and-drop-field'>
+        Перетащите сюда файл
+    </div>
+}
--- src/components/Modal/PrepareUploadMapModal.tsx
+++ src/components/Modal/PrepareUploadMapModal.tsx
@@ -1,10 +1,11 @@
-import { SyntheticEvent, useEffect, useRef, useState } from "react";
+import { SyntheticEvent, useEffect, useRef, useState, useMemo } from "react";
 import { Button, Dropdown, Form, Grid, Header, Modal } from "semantic-ui-react";
 
 import { Category } from "../../models/rest/Category";
 import { Flags } from "../../models/rest/Flags";
 import { AdditionalFlags, MapService } from "../../services/MapService";
 import { useCategoryFilter } from "../../hooks/useCategoryFilter";
+import { DragAndDropField } from "./DragAndDropField";
 
 interface PrepareUploadMapModalProps {
   onMapSelected: (
@@ -27,6 +28,9 @@ function PrepareUploadMapModal({
   const [loadingError, setLoadingError] = useState<boolean>(false);
 
   const [selectedCategories, setSelectedCategories] = useState<any>([]);
+
+  const [isDragging, setDragging] = useState(false);
+  const dragCounter = useMemo(() => ({ value: 0 }), []);
 
   useEffect(() => {
     const mapService = new MapService();
@@ -56,61 +60,122 @@ function PrepareUploadMapModal({
     onMapSelected(files, flags, {});
   };
 
+  const handleDrag = (ev: React.DragEvent<HTMLDivElement>) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+  };
+
+  const handleDragEnter = (ev: React.DragEvent<HTMLDivElement>) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    dragCounter.value++;
+
+    const { items } = ev.dataTransfer;
+    if (items?.[0].kind !== "file") {
+      return;
+    }
+
+    if (items[0] && selectedCategories.length) {
+      setDragging(true);
+    }
+  };
+
+  const handleDragExit = (ev: React.DragEvent<HTMLDivElement>) => {
+
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    dragCounter.value--;
+    if (dragCounter.value === 0) {
+
+      setDragging(false);
+    }
+  };
+
+  const handleDrop = (ev: React.DragEvent<HTMLDivElement>) => {
+
+
+    dragCounter.value = 0;
+    setDragging(false);
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    if (!selectedCategories.length) return;
+
+    const file = ev.dataTransfer.files[0];
+    if (file && (file.name.endsWith('.w3x') || file.name.endsWith('.w3m'))) {
+      emitMapSelected(ev.dataTransfer.files);
+    }
+  };
+
   return (
     <Modal open={open} onClose={onClose}>
       <Modal.Header>Выберите карту</Modal.Header>
       <Modal.Content>
-        <Modal.Description>
-          <Header>Какую карту загрузить?</Header>
-          <p>
-            Укажите путь до w3x карты. Обычно карты находятся в папке maps,
-            которая находится там где Warcraft III.
-          </p>
-          <Form>
-            <Form.Field>
-              <label>Выберите до 5 категорий для карты</label>
-              <Dropdown
-                fluid
-                multiple
-                placeholder="Категории"
-                selection
-                options={dropdownOptions}
-                error={loadingError}
-                loading={loadding}
-                onChange={(e, p) => {
-                  setSelectedCategories(p.value);
-                }}
-                value={selectedCategories}
-              />
-            </Form.Field>
-            <Form.Field>
-              <Grid>
-                <Grid.Row centered>
-                  <Button
-                    as={"a"}
-                    content="Выбрать карту"
-                    icon="file"
-                    disabled={!selectedCategories.length}
-                    size="big"
-                    onClick={() => {
-                      fileInput.current.click();
-                    }}
-                  />
-                  <input
-                    multiple
-                    accept=".w3x , .w3m"
-                    onChange={(e) => {
-                      emitMapSelected(e.target.files);
-                    }}
-                    type="file"
-                    hidden
-                    ref={fileInput}
-                  />
-                </Grid.Row>
-              </Grid>
-            </Form.Field>
-          </Form>
-        </Modal.Description>
+        <div
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragExit}
+          onDragOver={handleDrag}
+          onDrop={handleDrop}
+        >
+          <Modal.Description>
+            <Header>Какую карту загрузить?</Header>
+            <p>
+              Укажите путь до w3x карты. Обычно карты находятся в папке maps,
+              которая находится там где Warcraft III.
+            </p>
+            <Form>
+              <Form.Field>
+                <label>Выберите до 5 категорий для карты</label>
+                <Dropdown
+                  fluid
+                  multiple
+                  placeholder="Категории"
+                  selection
+                  options={dropdownOptions}
+                  error={loadingError}
+                  loading={loadding}
+                  onChange={(e, p) => {
+                    setSelectedCategories(p.value);
+                  }}
+                  value={selectedCategories}
+                />
+              </Form.Field>
+
+              {isDragging ? (
+                <DragAndDropField />
+              ) : (
+                <Form.Field>
+                  <Grid>
+                    <Grid.Row centered>
+                      <Button
+                        as={"a"}
+                        content="Выбрать карту"
+                        icon="file"
+                        disabled={!selectedCategories.length}
+                        size="big"
+                        onClick={() => {
+                          fileInput.current.click();
+                        }}
+                      />
+                      <input
+                        multiple
+                        accept=".w3x , .w3m"
+                        onChange={(e) => {
+                          emitMapSelected(e.target.files);
+                        }}
+                        type="file"
+                        hidden
+                        ref={fileInput}
+                      />
+                    </Grid.Row>
+                  </Grid>
+                </Form.Field>
+              )}
+            </Form>
+          </Modal.Description>
+        </div>
       </Modal.Content>
     </Modal>
   );
