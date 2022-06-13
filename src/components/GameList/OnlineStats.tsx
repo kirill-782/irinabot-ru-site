cfa683545b2cab15571927092a79d9e5c99981a9
Kirill <kirill-782@yandex.ru>
2022-06-13 13:44:59 +0300

    add new pvpgn realms

--- src/components/GameList/OnlineStats.tsx
+++ src/components/GameList/OnlineStats.tsx
@@ -15,10 +15,16 @@ interface OnlineStatsProps {
 const realmToCategory = {
   "178.218.214.114": "iccup",
   connector: "connector",
+  "pvpgn.onligamez.ru": "ozbnet",
+  "Rubattle.net": "rubattlenet",
+  "server.eurobattle.net": "eurobattlenet"
 };
 
 const categoryToString = {
   iccup: "Игроков с iCCup",
+  ozbnet: "Игроков с OZBnet",
+  rubattlenet: "Игроков с RuBattle.Net",
+  eurobattlenet: "Игроков с EuroBattle.Net",
   connector: "Игроков с IrInA Connector	",
   other: "Игроки с остальных платфром	",
   all: "Всего",
@@ -26,7 +32,7 @@ const categoryToString = {
   started: "Начатые игры",
 };
 
-const order = ["all", "lobby", "started", "connector", "iccup", "other"];
+const order = ["all", "lobby", "started", "connector", "iccup", "ozbnet", "rubattlenet", "eurobattlenet", "other"];
 
 function OnlineStats({ gameList }: OnlineStatsProps) {
   const gameStats = useMemo<OnlineStatsRow[]>(() => {
