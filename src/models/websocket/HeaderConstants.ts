4e45e47e9f769c16bd619d4cbacbaf9feb731301
Kirill <kirill-782@yandex.ru>
2022-08-02 22:55:35 +0300

    Syntax fix

--- src/models/websocket/HeaderConstants.ts
+++ src/models/websocket/HeaderConstants.ts
@@ -22,7 +22,7 @@ export const DEFAULT_CONTEXT_HEADER_CONSTANT = 0x01;
 // Server answer constant
 export const DEFAULT_CONTEXT_WELCOME = 0x00;
 export const DEFAULT_GAME_LIST = 0x01;
-export const DEFAULT_UDP_ANSWER = 0x03;``
+export const DEFAULT_UDP_ANSWER = 0x03;
 export const DEFAULT_CREATE_GAME_RESPONSE = 0x04;
 export const DEFAULT_WEBSOCKET_CONNECT_STATS = 0x05;
 export const DEFAULT_NEW_MESSAGE = 0x0c;
