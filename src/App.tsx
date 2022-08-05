9e63aeb8e915e15ece90a6841413c4cc0aa55240
Alex Space <cometospace689@gmail.com>
2022-08-05 13:47:33 +0600

    Changed props value in RegisterAccountModal

--- src/App.tsx
+++ src/App.tsx
@@ -97,7 +97,7 @@ function App() {
                   <Route path="/oauth" element={<OauthStubPage />} />
                 </Routes>
                 <RegisterAccountModal
-                  open={true}
+                  open={needRegisterModal}
                   onApprove={() => {
                     authDispatcher({ action: "setForce", payload: true });
                   }}
