f5ba8ae336b9823aaaae5d566dcd99c86c40ed9d
Kirill <kirill-782@yandex.ru>
2022-09-08 22:13:59 +0300

    Enter games with password

--- src/components/GameList/ConnectorAddButton.tsx
+++ src/components/GameList/ConnectorAddButton.tsx
@@ -1,5 +1,5 @@
-import { Button } from "semantic-ui-react";
-import React, { useContext } from "react";
+import { Button, Form, Icon, Modal } from "semantic-ui-react";
+import React, { useContext, useState } from "react";
 import { WebsocketContext } from "../../context";
 import { AuthContext } from "./../../context/index";
 import { GameListGame } from "../../models/websocket/ServerGameList";
@@ -13,15 +13,23 @@ function ConnectorAddButton({ game }: ConnectorAddButtonProps) {
   const sockets = useContext(WebsocketContext);
   const auth = useContext(AuthContext).auth;
 
-  const requestConnectorGame = () => {
+  const [passwordModalOpen, setPasswordModalOpen] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const requestConnectorGame = (password?: string) => {
     const converter = new ClientRequestUDPGameConverter();
     sockets.ghostSocket.send(
       converter.assembly({
         gameId: game.gameCounter,
         isPrivateKey: false,
-        password: "",
+        password: password || "",
       })
     );
+  };
+
+  const onButtonClick = () => {
+    if (game.gameFlags.hasPassword) setPasswordModalOpen(true);
+    else requestConnectorGame();
   };
 
   const isEnabled =
@@ -30,14 +38,51 @@ function ConnectorAddButton({ game }: ConnectorAddButtonProps) {
   if (game.gameFlags.started) return null;
 
   return (
-    <Button
-      icon="gamepad"
-      disabled={!isEnabled}
-      color={isEnabled ? "green" : "red"}
-      basic
-      size="mini"
-      onClick={requestConnectorGame}
-    />
+    <>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <Modal
+          open={passwordModalOpen}
+          closeIcon
+          onClose={() => {
+            setPasswordModalOpen(false);
+          }}
+        >
+          <Modal.Header>Вход в закрытую игру</Modal.Header>
+          <Modal.Content>
+            <Form>
+              <Form.Input
+                label="Пароль"
+                value={password}
+                onChange={(_, data) => {
+                  setPassword(data.value);
+                }}
+              ></Form.Input>
+              <Form.Button
+                color="green"
+                onClick={() => {
+                  requestConnectorGame(password);
+                  setPasswordModalOpen(false);
+                }}
+              >
+                <Icon name="check"></Icon>Войти
+              </Form.Button>
+            </Form>
+          </Modal.Content>
+        </Modal>
+      </div>
+      <Button
+        icon={game.gameFlags.hasPassword ? "lock" : "gamepad"}
+        disabled={!isEnabled}
+        color={isEnabled ? "green" : "red"}
+        basic
+        size="mini"
+        onClick={onButtonClick}
+      />
+    </>
   );
 }
 
