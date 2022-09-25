fe403e81acaf54833b307e6ba5042b63a78a825c
Kirill <kirill-782@yandex.ru>
2022-09-25 23:37:40 +0300

    Favorite list prepare

---
+++ src/utils/FavoriteMaps.ts
@@ -0,0 +1,22 @@
+export namespace FavoriteMaps {
+  const localStorageMapKey = "fav-maps";
+
+  export const addMap = (id: number) => {
+    const maps = listMaps().add(id);
+    localStorage.setItem(localStorageMapKey, JSON.stringify(Array.from(maps)));
+  };
+  export const removeMap = (id: number) => {
+    const maps = listMaps();
+    maps.delete(id);
+    localStorage.setItem(localStorageMapKey, JSON.stringify(Array.from(maps)));
+  };
+  export const cleanMaps = () => {
+    localStorage.setItem(localStorageMapKey, JSON.stringify([]));
+  };
+
+  export const listMaps = (): Set<number> => {
+    return new Set<number>(
+      JSON.parse(localStorage.getItem(localStorageMapKey) || "[]")
+    );
+  };
+}
