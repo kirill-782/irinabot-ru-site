4381e24051d800b02d8c502cce0e30afd8bcd4c9
Ivan <whitestranger7@gmail.com>
2024-07-10 20:46:47 +0200

    fix: improve chat header for long nicknames (#13)

---
+++ src/components/Chat/styles.ts
@@ -0,0 +1,9 @@
+import { CSSProperties } from "react";
+
+export const iconChatStyles: CSSProperties = {
+  cursor: "pointer",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}
