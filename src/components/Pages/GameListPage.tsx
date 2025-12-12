de7f9a3383437e6f62919a784f86c0616b07eda8
Maxim <64643017+p1nwl@users.noreply.github.com>
2025-12-12 19:22:20 +0500

    Удалена кнопка режима копирования

--- src/components/Pages/GameListPage.tsx
+++ src/components/Pages/GameListPage.tsx
@@ -110,16 +110,6 @@ function GameListPage() {
                                 size="large"
                             />
                         )}
-                        <Button
-                            floated="right"
-                            basic
-                            icon="copy"
-                            color={runtimeContext.linkCopyMode.copy ? "green" : undefined}
-                            size="large"
-                            onClick={() => {
-                                runtimeContext.linkCopyMode.setCopy((copy) => !copy);
-                            }}
-                        />
 
                         <Button
                             basic
